c2b0d92b04af816457c7c1d7df189dcb338eac64
RomanAli2 <romanali87866677@gmail.com>
2026-01-22 03:25:28 -0800

    first

--- src/components/Project-page.vue
+++ src/components/Project-page.vue
@@ -8,7 +8,7 @@
     <div class="projects flex justify-center gap-15 flex-wrap">
 <div class="div bg-slate-800 h-150 flex justify-center rounded-xl  text-center w-80 shadow-md shadow-blue-400 transition duration-500 hover:scale-105 hover:shadow-blue-400 hover:shadow-xl">
 <div class="w-70 h-50  py-4">
-  <img src="/image/amazora-teal.vercel.app_(iPad Pro).png" alt="project" class="rounded-md opacity-60"><br>
+  <img  src="/image/amazora-teal.vercel.app_(iPad Pro).png" alt="project" class="rounded-md opacity-60"><br>
   <h2 class="text-xl font-bold">Amazora Web</h2><br>
   <p>Amazora is a modern e-commerce web application designed with a clean UI and smooth user experience. It includes product listings, category filtering, add-to-cart functionality, and responsive layouts for all devices. This project is built using frontend technologies with a focus on performance, usability, and real-world shopping flow.</p><a target="_blank" href="https://amazora-teal.vercel.app/">
   <button class="px-7 py-3 text-xl rounded-xl cursor-pointer mt-2 bg-blue-500 text-white border border-blue-500"><i class="fa-solid fa-tv"></i>Demo</button></a>
@@ -19,6 +19,14 @@
   <img src="/image/localhost_5173_(iPad Pro).png" alt="project" class="rounded-md opacity-60"><br>
   <h2 class="text-xl font-bold">Phone Shop</h2><br>
   <p>This phone shop website was built to showcase mobile phones with a clean and user-friendly interface. It includes product cards, pricing details, and a responsive layout for mobile, tablet, and desktop screens. The project focuses on modern frontend design and smooth browsing experience..</p><a target="_blank" href="https://mega-mart-liard.vercel.app/">
+  <button class="px-7 py-3 text-xl rounded-xl mt-2 cursor-pointer bg-blue-500 text-white border border-blue-500"><i class="fa-solid fa-tv"></i>Demo</button></a>
+</div>
+</div>
+<div class="div bg-slate-800 h-150 flex justify-center rounded-xl  text-center w-80 shadow-md shadow-blue-400 transition duration-500 hover:scale-105 hover:shadow-blue-400 hover:shadow-xl">
+<div class="w-70  h-50 py-4">
+  <img src="/image/Untitled.png" alt="project" class="rounded-md opacity-60"><br>
+  <h2 class="text-xl font-bold">Weatherly</h2><br>
+  <p>Weatherly is a modern weather forecast web application designed with a clean UI and smooth user experience. It allows users to search for cities and view real-time weather details such as temperature, humidity, wind speed, and weather conditions. The application is fully responsive and built with Vue.js and Tailwind CSS, focusing on performance and usability.</p><a target="_blank" href="https://mega-mart-liard.vercel.app/">
   <button class="px-7 py-3 text-xl rounded-xl mt-2 cursor-pointer bg-blue-500 text-white border border-blue-500"><i class="fa-solid fa-tv"></i>Demo</button></a>
 </div>
 </div>
